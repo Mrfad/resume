727f42d8436a24d27f80bde5f114673ad2b9e5e7
mrfad <fady.ghalayiny@outlook.com>
2021-09-19 06:54:07 +0300

    changed emailbackend in settings and added timestamp in contact models

--- main/models.py
+++ main/models.py
@@ -89,6 +89,7 @@ class Contact(models.Model):
     email = models.EmailField()
     subject = models.CharField(max_length=150)
     message = models.TextField()
+    message_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return  self.name
